35aca6088f263f4689483fa3584b43c35873a9e5
grawitti <acinonyx85@gmail.com>
2018-10-31 11:43:04 +0500

    Ex. 16.2 4x4Buttons

--- GUI/My_window.cpp
+++ GUI/My_window.cpp
@@ -5,6 +5,13 @@ My_window::My_window(Point xy, int w, int h, const string &title)
       next_button{Point{x_max() - 150, 0}, 70, 20, "Next", cb_next},
       quit_button{Point{x_max() - 70, 0}, 70, 20, "Quit", cb_quit}
 {
+    create_buttons();
+    
+    for(int i = 0; i < buttons.size(); ++i)
+    {
+        attach(buttons[i]);
+    }
+    
     attach(next_button);
     attach(quit_button);
 }
@@ -18,7 +25,26 @@ void My_window::next()
 {
     redraw();
 }
+void My_window::create_buttons()
+{
+    int xoffset = 100;
+    int yoffset = 100;
+    int b_size = 40;
+    int i_label = 0;
+    Point first {xoffset, yoffset};
+    // Create 4x4 buttons set
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            buttons.push_back(new Button{first+Point{b_size*j,b_size*i},
+                                        b_size,b_size,to_string(i_label),cb_button});
+            ++i_label;
+        } 
+    }
+}
 
 //Other buttons callbacks
 void My_window::cb_next(Address, Address pw) { reference_to<My_window>(pw).next(); }
 void My_window::cb_quit(Address, Address pw) { reference_to<My_window>(pw).quit(); }
+void My_window::cb_button(Address, Address pw) { reference_to<My_window>(pw).button_pressed(); }
--- GUI/My_window.h
+++ GUI/My_window.h
@@ -18,15 +18,19 @@ struct My_window : Graph_lib::Window {
     My_window(Point xy, int w, int h, const string& title );
 private:
 
+    Vector_ref<Button> buttons;
     Button next_button;     // the "next" button
     Button quit_button;
 
     //Other buttons
     void next();            // action to be done when next_button is pressed
     void quit();
+    void create_buttons();
+    void button_pressed() {buttons[0].label = to_string(buttons[0].loc.x); redraw();}
 
     static void cb_next(Address, Address);
     static void cb_quit(Address, Address);
+    static void cb_button(Address, Address);
 };
 //------------------------------------------------------------------------------
 #endif // MY_WINDOW_GUARD
