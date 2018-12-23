56c7b266b47cbaacfe0114870a92d81313dc9da7
grawitti <acinonyx85@gmail.com>
2018-12-23 17:04:04 +0500

    T. 19.1-13 S

--- working/main.cpp
+++ working/main.cpp
@@ -10,23 +10,59 @@
 
 using namespace std;
 
-template<typename T> struct S {
+template<typename T> 
+struct S {
 	public:
 		S(T v) :val(v) {}
-		T& get() { return val; }
+		T& get();
+		const T& get() const;
+		// void read_val();
+
+		// void set(T v) { val = v; }
+		T& operator=(const T& v);
 	private:
 		T val;
 };
 
+template<typename T> 
+T& S<T>::get() { return val; }
+
+template<typename T> 
+const T& S<T>::get() const { return val; }
+
+template<typename T> 
+T& S<T>::operator=(const T& v) {
+	if(this==&v) return *this;
+	val = v.val;
+	return *this;
+}
+
+template<typename T> 
+void read_val(T& v) {
+	cin >> v;
+	if(!cin) error("Data format.",v);
+}
 
 int main()
 {
 	try
 	{	
-		S<int> si{12};
-		S<char> sc{'c'};
-		S<double> sd{2.3};
-		S<string> ss{"string"};
+		int i;
+		read_val(i);
+		S<int> si{i};
+
+		char c;
+		read_val(c);
+		S<char> sc{c};
+
+		double d;
+		read_val(d);
+		S<double> sd{d};
+
+		string s;
+		read_val(s);
+		S<string> ss{s};
+
 		S<vector<int>> svi{{1,2,3}};
 
 		cout << si.get() << endl
@@ -37,7 +73,13 @@ int main()
 		for(int i : svi.get()) cout << i << ' ';
 		cout << endl;
 
-		cout << si.get() << endl;
+		S<int> si2{0};
+		si = si2;
+
+		cout << si.get() << endl
+			<< sc.get() << endl
+			<< sd.get() << endl
+			<< ss.get() << endl;
 
 		return 0;
 	}
