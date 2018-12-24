5fe9708e6315ce5b4d0a02e5a1892ccd8af75bab
grawitti <acinonyx85@gmail.com>
2018-12-24 16:37:25 +0500

    Ex.19.2 sum of mult

--- 19/Ex.19.1_f/main.cpp
+++ 19/Ex.19.1_f/main.cpp
@@ -3,51 +3,40 @@
     Author: Grawitti
 	Description: 
     Chapter 19 – Principles and Practice Using C++   
-	Exercise - 1
+	Exercise - 2
 */
 
 #include "../../std_lib_facilities.h"
 
 using namespace std;
 
-template<typename T> 
-vector<T> f(vector<T> v1, vector<T> v2) {
-	if(v1.size() < 1 || v1.size()!=v2.size()) error("Vector size error.",v1.size());
-	for(int i = 0; i<v1.size(); ++i) v1[i]+=v2[i];
-	return v1;
+template <typename T, typename U>
+T sum_of_mult(const vector<T> &vt, const vector<U> &vu) {	
+	if(vt.size<0 || vt.size()!=vu.size()) error("Size of vectors.");
+	T result=0;
+	for(int i=0; i<vt.size(); ++i) { result+=vt[i]*vu[i]; }
+	return result;
 }
 
 int main()
 {
 	try
-	{	
-		vector<int> vi_1 {1,1,1};
-		vector<int> vi_2 {3,3,3};
-		vector<int> vi_12 = f(vi_1,vi_2);
+	{
+		vector<int> vi_1{1, 1, 1};
+		vector<int> vi_2{3, 3, 3};
+		cout << sum_of_mult(vi_1, vi_2);
 
-		for(int i : vi_12) cout << i << ' ';
-		cout << endl;
+		vector<string> vs_1{"1", "1", "1"};
+		vector<string> vs_2{"3", "3", "3"};
+		cout << sum_of_mult(vs_1, vs_2);
 
-		vector<string> vs_1 {"1","1","1"};
-		vector<string> vs_2 {"3","3","3"};
-		vector<string> vs_12 = f(vs_1,vs_2);
+		vector<double> vd_1{1.1, 1.1, 1.1};
+		vector<double> vd_2{3.3, 3.3, 3.3};
+		cout << sum_of_mult(vd_1, vd_2);
 
-		for(string s : vs_12) cout << s << ' ';
-		cout << endl;	
-
-		vector<double> vd_1 {1.1,1.1,1.1};
-		vector<double> vd_2 {3.3,3.3,3.3};
-		vector<double> vd_12 = f(vd_1,vd_2);
-
-		for(double d : vd_12) cout << d << ' ';
-		cout << endl;	
-		
-		vector<char> vc_1 {'1','1','1'};
-		vector<char> vc_2 {'3','3','3'};
-		vector<char> vc_12 = f(vc_1,vc_2);
-
-		for(char c : vc_12) cout << c << ' ';
-		cout << endl;
+		vector<char> vc_1{'1', '1', '1'};
+		vector<char> vc_2{'3', '3', '3'};
+		cout << sum_of_mult(vc_1, vc_2);
 
 		return 0;
 	}
